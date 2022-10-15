6615e28cf521c0f230dcb34f840bb4dedaf94392
Mazunin <mazunin.k@gmail.com>
2022-10-15 20:48:30 +0500

    add MaxNum3

---
+++ MaxNum3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Ищем максимальное из трех чисел");
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число ");
+int c = int.Parse(Console.ReadLine()!);
+if (b  <= a && a >= c)
+{
+    Console.Write("Максимальное из введенных чисел это ");
+    Console.Write(a);
+}
+if (a <= b && b >= c)
+{
+    Console.Write("Максимальное из введенных чисел это ");
+    Console.Write(b);
+}
+if (b <= c && c >= a)
+{
+    Console.Write("Максимальное из введенных чисел это ");
+    Console.Write(c);
+}
