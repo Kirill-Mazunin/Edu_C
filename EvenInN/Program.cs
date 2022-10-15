7a4c21f96db7201224014c8584d6064fa4a2c0a1
Mazunin <mazunin.k@gmail.com>
2022-10-15 20:58:05 +0500

    add EvenInN

---
+++ EvenInN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Четные числа от 1 до N");
+Console.Write("Введите число ");
+int count = 1;  
+int N = int.Parse(Console.ReadLine()!);
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(" ");
+        Console.Write(count);
+    }
+    count++;
+}
+
