873a79b68069c8e10c72a034da918f4c84a84dec
Mazunin <mazunin.k@gmail.com>
2022-10-19 22:39:13 +0500

    add DelSecondDigit

---
+++ DelSecondDigit/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+int index = 2;
+int temp = n;
+int res = 0;
+int step = 1;
+int count = 1;
+int length = 0;
+
+while (n > 0)
+{
+    n = n / 10;
+    length++;
+}
+
+while (temp != 0)
+{
+    if (count != length - index + 1)
+    {
+        res = res + ((temp % 10) * step);
+        step = step * 10;
+    }
+    temp = temp / 10;
+    count++;
+}
+
+Console.WriteLine(res);
