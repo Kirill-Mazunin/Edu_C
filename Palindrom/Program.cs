b6eed5e4a34d95fa5262af50be6a65168541bf37
Mazunin <mazunin.k@gmail.com>
2022-10-22 15:16:34 +0500

    add Palindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿int LengthNumber(int num)
+{
+    int length = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        length++;
+    }
+    return length;
+}
+
+int DigitInNum(int num, int index, int length)
+{
+    int count = 0;
+
+    while (count < length - index)
+    {
+        num = num / 10;
+        count++;
+    }
+    return (num % 10);
+}
+
+M:
+Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine()!);
+int len = LengthNumber(a);
+if (len < 5) { Console.WriteLine("Это число не пятизначное"); goto M; }
+int digit1 = DigitInNum(a, 1, len);
+int digit2 = DigitInNum(a, 2, len);
+int digit4 = DigitInNum(a, 4, len);
+int digit5 = DigitInNum(a, 5, len);
+if (digit1 == digit5 && digit2 == digit4) Console.WriteLine("Да, это палиндром");
+else Console.WriteLine("Нет, это не палиндром");
