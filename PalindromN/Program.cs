1635430599638ed3515bd50765b58ed44e0a7059
Mazunin <mazunin.k@gmail.com>
2022-10-23 01:16:26 +0500

    add PalindromN

---
+++ PalindromN/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int reverse(int num, int cont)
+{
+    while (num > 0) 
+    { 
+        cont = cont * 10 + num % 10; 
+        num /= 10; 
+    }
+    return cont;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+int cont = reverse(num, 0);
+if (cont == num) Console.Write("Введенное число - палиндром");
+else Console.Write("Введенное число не является палиндромом");
