c69a4cd17b5b89ffca1b2ff743d7ae1738b17063
Mazunin <mazunin.k@gmail.com>
2022-10-23 16:23:27 +0500

    add DistanceND

---
+++ DistanceND/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними 
+// в N-мерном пространстве. 
+// Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+Double DistanceND(int N)
+{
+int count = 0;
+double cont = 0;
+while (count < N)
+{
+    Console.Write("Введите координату " + (count + 1) + " первой точки: ");
+    int x1 = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите координату " + (count + 1) + " второй точки: ");
+    int x2 = int.Parse(Console.ReadLine()!);
+    cont = cont + (Math.Pow((x2 - x1), 2));
+    count += 1;
+}
+return(Math.Round(Math.Sqrt(cont), 2)); 
+}
+
+Console.Write("Введите размерность пространства: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(DistanceND(N));
