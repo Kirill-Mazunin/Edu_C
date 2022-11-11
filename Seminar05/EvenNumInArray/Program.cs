965eb754ddb8d8ac792286eae81cd0e130a16cea
Mazunin <mazunin.k@gmail.com>
2022-11-11 23:22:29 +0500

    add EvenNumInArray

---
+++ Seminar05/EvenNumInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+}
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+void GetEvenInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) {count++;}  
+    }
+    Console.Write(count);
+}
+int [] array = new int[4];
+FillArray(array);
+PrintArray(array);
+GetEvenInArray(array);
