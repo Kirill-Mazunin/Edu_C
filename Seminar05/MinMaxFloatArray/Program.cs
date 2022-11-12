2112214a59a08cb1547bb7f8eb0d3949aaed961e
Mazunin <mazunin.k@gmail.com>
2022-11-12 22:22:06 +0500

    add minmaxfloatarray

---
+++ Seminar05/MinMaxFloatArray/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+void FillArray(float[] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 100);
+    }
+}
+void PrintArray(float[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+float MaxElementInArray(float[] array)
+{
+    float max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) { max = array[i]; }
+    }
+    return max;
+}
+float MinElementInArray(float[] array)
+{
+    float min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) { min = array[i]; }
+    }
+    return min;
+}
+
+float[] array = new float[5];
+
+FillArray(array);
+PrintArray(array);
+
+float min = MinElementInArray(array);
+float max = MaxElementInArray(array);
+Console.WriteLine();
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine("Разница между минимальным и максимальным элементом массива: " + (max - min));
