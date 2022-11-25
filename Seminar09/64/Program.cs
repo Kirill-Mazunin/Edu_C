59bff76248092f4ef25e321debb81ad31198ec9d
Mazunin <mazunin.k@gmail.com>
2022-11-26 01:23:14 +0500

    Seminar09

---
+++ Seminar09/64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string DecNum(int N)
+{
+ if (1 <= N) return $"{N} " + DecNum(N - 1);
+ else return String.Empty;
+}
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DecNum(N));
---
+++ Seminar09/66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRec(int M, int N)
+{
+ if (N < M) return 0;
+ else return M + SumRec(M + 1, N);
+}
+
+int M = int.Parse(Console.ReadLine()!);
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumRec(M, N)); 
